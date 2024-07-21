0767b267e060bb0b773e7ff7f68e6e20813e3b09
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-21 17:07:12 +0530

    Added Bg and write the slider function for song sliding

--- script.js
+++ script.js
@@ -1,55 +1,98 @@
-let isappended_newdiv = false;
 let is_playing_song = false;
-let curr_music_path = "";
+let curr_audio = "";
 
-document.addEventListener("click", function (event) {
-  
-
-  let data_recevied = event.target.textContent;
-  
-  if(isappended_newdiv == true){
-    let ele = document.querySelector('.current-song');
-    console.log(ele);
-    ele.innerHTML = data_recevied;
-  }else{
-    let newdiv = document.createElement('div');
-    newdiv.classList.add("current-song");
-    newdiv.innerHTML = data_recevied;
-    const parentdiv = document.querySelector('.footer-main');
-    parentdiv.appendChild(newdiv);
-    isappended_newdiv = true;
-  }
-
-  if(data_recevied == "One call away - By charlie puth"){
-    if(is_playing_song == false){
-      new Audio("/songs/one call away.mp3").play();
-      is_playing_song = true;
-    }else{
-      
-      // new Audio(curr_music_path).currentTime = 0;
-      new Audio(curr_music_path).pause();
-      new Audio("/songs/one call away.mp3").play();
-      current_playing_song = true;
-      curr_music_path = "/songs/one call away.mp3";
+function range_slider(music) {
+  music.addEventListener("timeupdate", () => {
+    const percent = (music.currentTime / music.duration) * 100;
+    seeker.value = percent;
+    if (percent == 100) {
+      seeker.value = 0;
+      let div_to_be_remove = document.querySelector(".current-song");
+      div_to_be_remove.remove();
+      is_playing_song = false;
     }
-    
-  }else if(data_recevied == "Chahun main ya na - Ashiqui 2"){
-    if(is_playing_song == true){
-      // new Audio(curr_music_path).currentTime = 0;
-      new Audio(curr_music_path).pause();
-      new Audio("/songs/chahu mein ya na.mp3").play();
-      curr_music_path = "/songs/chahu mein ya na.mp3";
+  });
+}
+
+const song1 = new Audio("/songs/one call away.mp3");
+const song2 = new Audio("/songs/chahu mein ya na.mp3");
+const song3 = new Audio("/songs/mercy.mp3");
+const song4 = new Audio("/songs/malhari.mp3");
+const song5 = new Audio("/songs/act my age.mp3");
+const song6 = new Audio("/songs/pi jaun.mp3");
+
+let song_elements = document.querySelectorAll(".songs");
+for (i = 0; i < song_elements.length; i++) {
+  song_elements[i].addEventListener("click", function (event) {
+    let data_recevied = event.target.textContent;
+
+    if (is_playing_song == true) {
+      let ele = document.querySelector(".current-song");
+      ele.innerHTML = data_recevied;
+      curr_audio.pause();
+      curr_audio.currentTime = 0;
+      if (data_recevied == "One call away - By charlie puth") {
+        song1.play();
+        curr_audio = song1;
+        range_slider(song1);
+      } else if (data_recevied == "Chahun main ya na - Ashiqui 2") {
+        song2.play();
+        curr_audio = song2;
+        range_slider(song2);
+      } else if (data_recevied == "Mercy - By Badshah") {
+        song3.play();
+        curr_audio = song3;
+        range_slider(song3);
+      } else if (data_recevied == "Malhari - Bajirao Mastani") {
+        song4.play();
+        curr_audio = song4;
+        range_slider(song4);
+      } else if (data_recevied == "Act my age - By One direction") {
+        song5.play();
+        curr_audio = song5;
+        range_slider(song5);
+      } else if (data_recevied == "Pi jaun - Farhan Saeed") {
+        song6.play();
+        curr_audio = song6;
+        range_slider(song6);
+      }
+    } else {
+      let newdiv = document.createElement("div");
+      newdiv.classList.add("current-song");
+      newdiv.innerHTML = data_recevied;
+      const parentdiv = document.querySelector(".footer-main");
+      parentdiv.appendChild(newdiv);
+      if (data_recevied == "One call away - By charlie puth") {
+        song1.play();
+        range_slider(song1);
+        curr_audio = song1;
+        is_playing_song = true;
+      } else if (data_recevied == "Chahun main ya na - Ashiqui 2") {
+        song2.play();
+        range_slider(song2);
+        curr_audio = song2;
+        is_playing_song = true;
+      } else if (data_recevied == "Mercy - By Badshah") {
+        song3.play();
+        range_slider(song3);
+        curr_audio = song3;
+        is_playing_song = true;
+      } else if (data_recevied == "Malhari - Bajirao Mastani") {
+        song4.play();
+        curr_audio = song4;
+        range_slider(song4);
+        is_playing_song = true;
+      } else if (data_recevied == "Act my age - By One direction") {
+        song5.play();
+        range_slider(song5);
+        curr_audio = song5;
+        is_playing_song = true;
+      } else if (data_recevied == "Pi jaun - Farhan Saeed") {
+        song6.play();
+        range_slider(song6);
+        curr_audio = song6;
+        is_playing_song = true;
+      }
     }
-    new Audio("/songs/chahu mein ya na.mp3").play();
-    is_playing_song = true;
-    curr_music_path = "/songs/chahu mein ya na.mp3";
-  }else if(data_recevied == "Mercy - By Badshah"){
-    new Audio("/songs/mercy.mp3").play();
-  }else if(data_recevied == "Malhari - Bajirao Mastani"){
-    new Audio("/songs/malhari.mp3").play();
-  }else if(data_recevied == "Act my age - By One direction"){
-    new Audio("/songs/act my age.mp3").play();
-  }else if(data_recevied == "Pi jaun - Farhan Saeed"){
-    new Audio("/songs/pi jaun.mp3").play();
-  }
-});
+  });
+}
