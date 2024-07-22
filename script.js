c8fe69f67cf0e3a6aa0803e65dfa0211b7745571
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-22 10:21:03 +0530

    Started working on custom slider seeking

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let is_playing_song = false;
 let curr_audio = "";
 
+let seek = document.getElementById("#seeker");
+
+seek.addEventListener("change", function(){
+  const clickpoint = seek.value;
+  curr_audio.currentTime = (curr_audio.duration || 0) * clickpoint;
+})
+
 function range_slider(music) {
   music.addEventListener("timeupdate", () => {
     const percent = (music.currentTime / music.duration) * 100;
