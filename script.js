5530a248f4bcbea7004a3ea7a224e6bf737082fd
Nitin shukla <nitin.shukla12004@gmail.com>
2024-07-24 16:46:05 +0530

    Added functionlity of backward play

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let is_playing_song = false;
 let curr_audio = "";
 let selected_user_song = "";
 let is_newsong_selected = false;
+let is_seeker_100attained = false;
+let song_index = 0;
 
 let seek = document.getElementById("seeker");
 
@@ -13,14 +15,35 @@ seek.addEventListener("change", function () {
 function range_slider(music) {
   music.addEventListener("timeupdate", () => {
     seeker.value = (music.currentTime / music.duration) * 100;
-    if (seeker.value == 100) {
+    if (seeker.value == 100 && is_seeker_100attained == false) {
       seeker.value = 0;
       let div_to_be_remove = document.querySelector(".current-song");
       div_to_be_remove.remove();
       is_playing_song = false;
+      is_newsong_selected = false;
+      play_pause_element.classList.remove("fa-circle-pause");
+      play_pause_element.classList.add("fa-circle-play");
+      is_seeker_100attained = true;
     }
   });
 }
+
+let backward_play_btn = document.querySelector(".play-backward");
+let forward_play_btn = document.querySelector(".play-backward");
+
+backward_play_btn.addEventListener("click", function () {
+  if ((song_index == 1)) {
+    let ele = document.querySelector(".current-song");
+    ele.innerHTML = "Pi jaun - Farhan Saeed";
+    curr_audio.pause();
+    is_playing_song = false;
+    curr_audio.currentTime = 0;
+    seeker.value = 0;
+    play_pause_element.classList.remove("fa-circle-pause");
+    play_pause_element.classList.add("fa-circle-play");
+    selected_user_song = song6;
+  }
+});
 
 const play_pause_element = document.querySelector(".playbtn");
 
@@ -60,21 +83,27 @@ for (i = 0; i < song_elements.length; i++) {
       if (data_recevied == "One call away - By charlie puth") {
         selected_user_song = song1;
         curr_audio = song1;
+        song_index = 1;
       } else if (data_recevied == "Chahun main ya na - Ashiqui 2") {
         selected_user_song = song2;
         curr_audio = song2;
+        song_index = 2;
       } else if (data_recevied == "Mercy - By Badshah") {
         selected_user_song = song3;
         curr_audio = song3;
+        song_index = 3;
       } else if (data_recevied == "Malhari - Bajirao Mastani") {
         selected_user_song = song4;
         curr_audio = song4;
+        song_index = 4;
       } else if (data_recevied == "Act my age - By One direction") {
         selected_user_song = song5;
         curr_audio = song5;
+        song_index = 5;
       } else if (data_recevied == "Pi jaun - Farhan Saeed") {
         selected_user_song = song6;
         curr_audio = song6;
+        song_index = 6;
       }
     } else {
       let newdiv = document.createElement("div");
@@ -86,21 +115,27 @@ for (i = 0; i < song_elements.length; i++) {
       if (data_recevied == "One call away - By charlie puth") {
         selected_user_song = song1;
         curr_audio = song1;
+        song_index = 1;
       } else if (data_recevied == "Chahun main ya na - Ashiqui 2") {
         selected_user_song = song2;
         curr_audio = song2;
+        song_index = 2;
       } else if (data_recevied == "Mercy - By Badshah") {
         selected_user_song = song3;
         curr_audio = song3;
+        song_index = 3;
       } else if (data_recevied == "Malhari - Bajirao Mastani") {
         selected_user_song = song4;
         curr_audio = song4;
+        song_index = 4;
       } else if (data_recevied == "Act my age - By One direction") {
         selected_user_song = song5;
         curr_audio = song5;
+        song_index = 5;
       } else if (data_recevied == "Pi jaun - Farhan Saeed") {
         selected_user_song = song6;
         curr_audio = song6;
+        song_index = 6;
       }
     }
   });
